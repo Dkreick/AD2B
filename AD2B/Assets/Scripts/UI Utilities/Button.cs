ca470380109a3b0588ef1bdb7134ec5a19762f56
Dkreick <damian.kreick@gmail.com>
2018-05-07 23:55:55 -0300

    Implements functionality on MainMenu Panel right buttons

---
+++ AD2B/Assets/Scripts/UI Utilities/Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    private string _url;
+
+    public void OpenURL(string _url)
+    {
+        Application.OpenURL(_url);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
