33ce8d06f9870f839620058863cd848905d3077b
DKreick <damian.kreick@gmail.com>
2018-05-09 14:14:04 -0300

    Adds music on MainMenu, StatsMenu and CombatMenu

---
+++ AD2B/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public float secondsBeforePlay;
+    private bool play;
+    private float musicVolume;
+    private float fxVolume;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Start()
+    {
+        StartCoroutine("Wait");
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(secondsBeforePlay);
+        GetComponent<AudioSource>().Play();
+    }
+}
