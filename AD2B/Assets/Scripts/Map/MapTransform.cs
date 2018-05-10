6da3de82f1933ace09b64ece41066b1f479c73d7
Dkreick <damian.kreick@gmail.com>
2018-05-10 13:40:23 -0300

    Implements simple map movement

---
+++ AD2B/Assets/Scripts/Map/MapTransform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTransform : MonoBehaviour
+{
+    public float minScale;
+    public float maxScale;
+    public float amountToScale;
+
+    public void ScaleMap()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x += amountToScale;
+            scale.y += amountToScale;
+            transform.localScale = scale;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x -= amountToScale;
+            scale.y -= amountToScale;
+            transform.localScale = scale;
+        }
+
+        transform.localScale = new Vector3(
+            Mathf.Clamp(transform.localScale.x, minScale, maxScale),
+            Mathf.Clamp(transform.localScale.y, minScale, maxScale),
+            Mathf.Clamp(transform.localScale.z, minScale, maxScale)
+        );
+    }
+}
---
+++ AD2B/Assets/Scripts/Map/MovePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 target;
+    public float speed;
+    public static bool isMoving;
+
+    void OnMouseDown()
+    {
+        isMoving = true;
+    }
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            player.transform.LookAt(target, Vector3.forward);
+            target.z = transform.position.z;
+            player.transform.position = Vector3.MoveTowards(player.transform.position, target, speed * Time.deltaTime);    
+        }
+
+        if (player.transform.position == target)
+        {
+            isMoving = false;
+        }
+    }
+}
