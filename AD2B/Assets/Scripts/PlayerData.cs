66b8eec9125b601627b3fc085f460f4714ee7855
Dkreick <damian.kreick@gmail.com>
2018-05-10 15:40:34 -0300

    MainMenu creates panel username

---
+++ AD2B/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public static string username = "Player";
+    public static int level;
+    public static int money;
+    public static float experience;
+    public static string charClass;
+
+}
