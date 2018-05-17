5827423db8aa7fa7dfe013ec02258b1c85650e4c
Dkreick <damian.kreick@gmail.com>
2018-05-17 12:09:54 -0300

    Adds fade in effect on main menu buttons and title

---
+++ AD2B/Assets/Scripts/UI Utilities/TextFadeIn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFadeIn : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    public float secondsToTriggerFade;
+    public float secondsToCompleteFade;
+
+    void Start()
+    {
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+
+        StartCoroutine("FadeIn");
+    }
+
+    IEnumerator FadeIn()
+    {
+        yield return new WaitForSeconds(secondsToTriggerFade);
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Time.deltaTime / secondsToCompleteFade;
+            yield return null;
+        }
+        canvasGroup.interactable = true;
+    }
+}
