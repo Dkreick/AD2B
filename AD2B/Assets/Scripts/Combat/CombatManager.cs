d5bee099937069e0a2214eb6625f134366eca5e4
Dkreick <damian.kreick@gmail.com>
2018-05-18 19:52:57 -0300

    Develops simple player attack

---
+++ AD2B/Assets/Scripts/Combat/CombatManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemy;
+    public float sliderValueSpeed;
+    public Slider enemyHealthBar;
+    public Slider enemyStaminaBar;
+    public Slider enemyAdrenalineBar;
+
+    public void Cover()
+    {
+
+    }
+
+    public void Inventory()
+    {
+
+    }
+
+    public void Attack()
+    {
+        float totalDamage = 10;
+        StartCoroutine(ChangeSliderValue(enemyHealthBar, totalDamage));
+    }
+
+    IEnumerator ChangeSliderValue(Slider bar, float newValue)
+    {
+        
+        float temp = bar.value - newValue / 100;
+        print(temp);
+        while (bar.value > temp)
+        {
+            bar.value -= sliderValueSpeed * Time.deltaTime;
+            yield return null;
+        }
+    }
+}
