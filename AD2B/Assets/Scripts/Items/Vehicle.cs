1a23c44b319408979ba33c59fa9172d815094a8b
DKreick <damian.kreick@gmail.com>
2018-08-09 17:10:49 -0300

    Adds vehicles as scriptable object

---
+++ AD2B/Assets/Scripts/Items/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = ("Item/Vehicle"))]
+public class Vehicle : ScriptableObject
+{
+	[SerializeField] GameObject weaponPrefab;
+	[SerializeField] string id;
+	[SerializeField] string description;
+	[SerializeField] int speed;
+	[SerializeField] int durability;
+	[SerializeField] int range;
+	[SerializeField] int trunkCapacity;
+	[SerializeField] float fightRisk;
+	[SerializeField] Sprite image;
+
+	public GameObject GetWeaponPrefab()
+	{
+		return weaponPrefab;
+	}
+}
